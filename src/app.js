256c1a3afa7cac1ac778ae5c9716ec4011893422
sonh <sonhoai272@gmail.com>
2017-05-06 14:10:12 +0700

    add redux and vidu ve redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const redux = require('redux')
 
 const App = () => (
     <div>
@@ -11,3 +12,20 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')// eslint-disable-line
 );
+
+//dispath la thuc hien phai truyen vao mot cai action
+
+const reducer = (state = 0, action) => {
+    if(action.type === 'THEM') return state + 1
+    return state
+}
+
+const store = redux.createStore(reducer)
+
+const state = store.getState()
+
+console.log(state)
+
+store.dispatch({type: 'THEM'})
+
+console.log(store.getState())
